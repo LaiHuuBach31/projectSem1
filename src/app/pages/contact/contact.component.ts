7963164f99eb46c2e643230b9e3c685b6ca05f4a
hung <hung03072003@gmail.com>
2022-10-05 14:31:41 +0700

    hh

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  formData = new FormGroup({
+    userName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    subject: new FormControl('', [
+      Validators.required
+    ]),
+    message: new FormControl('', [
+      Validators.required
+    ])
+  })
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  submitForm(): void {
+    if (this.formData.invalid){
+      return;
+    }
+    console.log(this.formData.value)
+  }
+  get form(): any {
+    return this.formData.controls;
+  }
 }
