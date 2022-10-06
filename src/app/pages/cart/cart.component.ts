a40ccd2678fb8c33e43ea404d99c656aa689a614
trkhduy <trkhduy003@gmail.com>
2022-10-06 13:52:15 +0700

    update_6-10

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -12,7 +12,8 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
   }
   mark(mark: any) {
-    let quantily: any = document.getElementById('quantily')
+    let quantily: any = document.getElementById('quantity')
+    let quantily_768: any = document.getElementById('quantity_768')
     if (mark == '-') {
       if (quantily.value > 1) {
         quantily.value = quantily.value - 1
@@ -20,6 +21,14 @@ export class CartComponent implements OnInit {
     } else {
       quantily.value = Number(quantily.value) + 1
     }
+    if (mark == '-') {
+      if (quantily_768.value > 1) {
+        quantily_768.value = quantily_768.value - 1
+      }
+    } else {
+      quantily_768.value = Number(quantily_768.value) + 1
+    }
+
   }
 
 }
